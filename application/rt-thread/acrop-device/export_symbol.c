fafdf5b4d11c49bbbf88b6ce8375cc1d062dcbb6
zhouyu <listzhouyu@gmail.com>
2025-08-30 19:45:58 +0800

    add export_symbol.c

---
+++ application/rt-thread/acrop-device/export_symbol.c
@@ -0,0 +1,10 @@
+#include <aic_core.h>
+#include <hal_wdt.h>
+
+RTM_EXPORT(rt_pin_mode);
+RTM_EXPORT(rt_pin_write);
+RTM_EXPORT(rt_pin_read);
+RTM_EXPORT(rt_pin_get);
+
+//rtc
+RTM_EXPORT(rt_hw_cpu_reset);
