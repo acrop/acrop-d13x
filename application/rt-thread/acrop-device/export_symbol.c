19b021d831dc7a8e582e2593d614e3332636e292
zhouyu <listzhouyu@gmail.com>
2025-09-13 15:54:06 +0800

    edit export_symbol.c

--- application/rt-thread/acrop-device/export_symbol.c
+++ application/rt-thread/acrop-device/export_symbol.c
@@ -1,5 +1,6 @@
-#include <aic_core.h>
-#include <hal_wdt.h>
+#include <drivers/pin.h>
+#include <rthw.h>
+#include <stdlib.h>
 
 RTM_EXPORT(rt_pin_mode);
 RTM_EXPORT(rt_pin_write);
@@ -8,3 +9,9 @@ RTM_EXPORT(rt_pin_get);
 
 //rtc
 RTM_EXPORT(rt_hw_cpu_reset);
+
+//memory
+RTM_EXPORT(malloc);
+RTM_EXPORT(free);
+RTM_EXPORT(realloc);
+RTM_EXPORT(calloc);
